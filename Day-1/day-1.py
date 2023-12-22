da38b3b81738ec064b55ae3ade5605a3e872507e
Shireesha TN <shireesha289@gmail.com>
2023-12-21 23:38:50 -0500

    variables creation, Data types

--- Day-1/day-1.py
+++ Day-1/day-1.py
@@ -1,3 +1,52 @@
-print("Hello World")
-msg = 'Welcome to Day 1 challenge'
-print ("msg:", msg)
+# print("Hello World")
+# msg = 'Welcome to Day 1 challenge'
+# print ("msg:", msg)
+
+# month="asswswsw"
+# day="ASasaa"
+# print(id(month))
+# print(id(day))
+
+
+# counter = 100          # Creates an integer variable
+# miles   = 1000.0       # Creates a floating point variable
+# name    = "Zara Ali"   # Creates a string variable
+
+# print (counter)
+# print (miles)
+# print (name)
+
+# # del counter
+# # print (counter)
+
+# a=10
+# b=10
+# c=10
+
+# a=b=c=10
+# print (a,b,c)
+
+# a,b,c = 10,20,30
+# print (a,b,c)
+
+
+# a=10
+# print("a",a)
+
+# price = 100
+# qty = 5
+# total = price*qty
+# print ("Total = ", total)
+
+# print ("My first program")
+# price = 100
+# qty = 5
+# total = prive*qty #Error in this statement
+# print ("Total = ", total)
+
+var1 = 1       # int data type
+var2 = True    # bool data type
+var3 = 10.023  # float data type
+var4 = 10+3j   # complex data type
+
+print(var1,var2,var3,var4)
