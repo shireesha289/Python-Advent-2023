b732b1397cc9af498917e5d7d3f3d8ca6de3f670
Shireesha TN <shireesha289@gmail.com>
2023-12-29 23:46:28 -0500

    Type Cast different types

---
+++ Day-3/day-3.py
@@ -0,0 +1,110 @@
+# a=10   # int object
+# b=10.5 # float object
+# c=a+b
+# print (c)
+
+# a=True;
+# b=10.5;
+# c=a+b;
+
+# print (c);
+
+# Python Explicit Casting
+
+# a = int(10.5) #converts a float object to int
+# print(a)
+
+# a = int(2*3.14) #expression results float, is converted to int
+# print(a)
+# print(type(a))
+
+
+
+# a=int(True)
+# print(a)
+
+# print(type(a))
+
+
+
+
+# a = int("100")
+# print(a)
+# print(type(a))
+
+# a = ("10"+"01")
+# a = int("10"+"01")
+# print(a)
+# print(type(a))
+
+# a = int("10.5")
+# print(a)
+
+# a = 9.99
+# print(type(a))
+
+# a = float(100)
+# print(type(a))
+
+# a = float("1,234.50")
+# print(type(a))
+
+# a = float(1)     # a will be 1.0
+# b = float(2.2)   # b will be 2.2
+# c = float("3.3") # c will be 3.3
+
+# print (a)
+# print (b)
+# print (c)
+
+# a = str(10)
+# print(type(a))
+ 
+# a=str(11.10)
+# print(type(a))
+
+
+# a = str(2/5)
+# print(type(a))
+
+a=[1,2,3,4,5]   # List Object
+b=(1,2,3,4,5)   # Tupple Object
+c="Hello"       # String Object
+
+# ### list() separates each character in the string and builds the list
+# obj=list(c)
+# print(obj)
+
+
+# ### The parentheses of tuple are replaced by square brackets
+# obj=list(b)
+# print(obj)
+
+
+### tuple() separates each character from string and builds a tuple of characters
+# obj=tuple(c)
+# print(obj)
+
+
+# ### square brackets of list are replaced by parentheses.
+# obj=tuple(a)
+# print(obj)
+
+### str() function puts the list and tuple inside the quote symbols.
+obj=str(a)
+print(obj)
+
+obj=str(b)
+print(obj)
+
+
+
+
+
+
+
+
+
+
+
+
